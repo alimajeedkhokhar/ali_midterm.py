88aec0715732791741e7cb5261d969b5fbc2fc02
alimajeedkhokhar <156563101+alimajeedkhokhar@users.noreply.github.com>
2024-01-21 10:15:03 +0500

    Add files via upload

---
+++ ALIMAJEED_MIDTERM.py
@@ -0,0 +1,14 @@
+#Q:1 AIRTHMETIC OPERATIONS
+firstnum=int(input("enter the first num: "))
+secondnum=int(input("enter the sec num: "))
+print("sum: ",firstnum+secondnum)
+
+
+#Q:1(2)
+firstnum=int(input("enter the first num: "))
+secondnum=int(input("enter the second num; "))
+print("subtract: ",firstnum-secondnum)
+
+
+
+
